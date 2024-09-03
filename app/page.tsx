5697bb43e8753d7a40d67fd1c6895c97b07b0611
RezaYes <rezamonochromics@gmail.com>
2024-09-03 10:03:58 +0700

    remove projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import Intro from '@/components/intro'
 import Venues from '@/components/venues'
 import NewsletterForm from '@/components/newsletter-form'
-import RecentPosts from '@/components/recent-posts'
-import RecentProjects from '@/components/recent-projects'
 import AudioPlayer from '@/components/AudioPlayer'
 import Carousel from '@/components/Carousel';
 
