4a215ea7db035c01cdd50642654b1fee613d29f9
RezaYes <rezamonochromics@gmail.com>
2024-09-03 10:45:16 +0700

    Ungraysacale intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -22,7 +22,7 @@ export default function Intro() {
       </div>
       <div className='relative'>
         <Image
-          className='flex-1 rounded-lg grayscale'
+          className='flex-1 rounded-lg'
           src={authorImage}
           alt='Jodie Kennedy'
           width={175}
